e27962bdab8d77749c36de5a801a33256a26bb31
Apoorv-Tripathi <apoorvtripathi895@gmail.com>
2026-05-26 21:04:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://unified-edu-platform-o47m.vercel.app/login',
+  origin: 'https://unified-edu-platform-o47m.vercel.app',
   credentials: true
 }));
 app.use(express.json());
